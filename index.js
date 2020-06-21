72665b179552a61b5c0a7ecd3d5c8ab117130d62
aslanator <aslanator@mail.ru>
2020-06-21 12:16:52 +0300

    add routes

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 3000;
 const fs = require('fs');
 const path = require('path');
 
+
+app.use('/static', express.static(path.resolve(__dirname, './static')));
+
 app.get('/:path/:id', (req, res) => {
     let file = path.resolve(__dirname, `./controllers/${req.params.path}/${req.params.id}.json`);
     fs.readFile(file, (err, data) => {
@@ -27,8 +30,12 @@ app.get('/:path', (req, res) => {
                 fs.readFile(path.resolve(dir, file), (err, data) => {
                     if(!err)
                         jsons.push(JSON.parse(data));
-                    if(key == files.length - 1)
-                        res.json(jsons);
+                    if(key == files.length - 1){
+                        if(jsons.length === 1)
+                            res.json(jsons[0]);
+                        else
+                            res.json(jsons);
+                    }
                 });
             }
         }
@@ -38,7 +45,6 @@ app.get('/:path', (req, res) => {
 
     }
 });
-
 
 
 app.listen(port, () => console.log(`Fake app listening at http://localhost:${port}`))
